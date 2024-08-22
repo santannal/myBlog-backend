61430976a530382f0410e3273924d6627015db66
Leonardo Sant'Anna <140214338+santannal@users.noreply.github.com>
2024-08-22 10:49:46 -0300

    configurando back end

---
+++ emailservice/src/main/java/com/models/EmailDetails.java
@@ -0,0 +1,33 @@
+package com.models;
+
+public class EmailDetails {
+
+    private String name;
+    private String email;
+    private String text;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+}
